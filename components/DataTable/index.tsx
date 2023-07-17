ccee043ea604f544a21bf38ed2bd72b021e560a0
Saurabh Sen <saurabh.sen.cs19@ggits.net>
2023-07-17 11:42:18 +0530

    project initialized and setup completed

---
+++ components/DataTable/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react'
+import { TableProps } from '@/types'
+
+const DataTable = ({caption} : TableProps) => {
+  return (
+    <TableContainer maxW='5xl' m="auto">
+      <Table variant='striped' colorScheme='teal'>
+        <TableCaption>{caption ?? 'Results'}</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>To convert</Th>
+            <Th>into</Th>
+            <Th isNumeric>multiply by</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>inches</Td>
+            <Td>millimetres (mm)</Td>
+            <Td isNumeric>25.4</Td>
+          </Tr>
+          <Tr>
+            <Td>feet</Td>
+            <Td>centimetres (cm)</Td>
+            <Td isNumeric>30.48</Td>
+          </Tr>
+          <Tr>
+            <Td>yards</Td>
+            <Td>metres (m)</Td>
+            <Td isNumeric>0.91444</Td>
+          </Tr>
+        </Tbody>
+        <Tfoot>
+          <Tr>
+            <Th>To convert</Th>
+            <Th>into</Th>
+            <Th isNumeric>multiply by</Th>
+          </Tr>
+        </Tfoot>
+      </Table>
+    </TableContainer>
+  )
+}
+
+export default DataTable
---
+++ types/index.ts
@@ -0,0 +1,7 @@
+export type TableProps = {
+    caption?: string;
+    rows: string[];
+    headerRow?: string[];
+    pagination?: boolean;
+    sorting?: boolean;
+}
